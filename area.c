5d579fe2b0ec6838d5e2d788e19623941e7745ff
Aarav-S0406 <aarav.s0406@gmail.com>
2025-08-13 10:43:00 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,15 @@
+ // comments added through double slash //
+
+ # include<stdio.h>
+
+ int main() {
+    int a,b ;
+    printf("enter a:");
+    scanf("%d", &a) ;
+
+    printf("enter b:") ;
+    scanf("%d", &b) ;
+
+    printf("area is:%d", a*b) ;
+    return 0 ;
+}
