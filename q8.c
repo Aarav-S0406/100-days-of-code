1faff15ccc700cb6d6077d254037edf081cdb7da
Aarav-S0406 <aarav.s0406@gmail.com>
2025-08-25 19:05:55 +0530

    Add files via upload

---
+++ q8.c
@@ -0,0 +1,28 @@
+//Q8: Write a program to find and display the sum of the first n natural numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+Sum=15
+
+Input 2:
+10
+Output 2:
+Sum=55
+
+*/
+
+#include<stdio.h>
+
+int main () {
+
+    int a=5;
+    printf("%d\n", 5+4+3+2+1);
+
+    int b=10;
+    printf("%d\n", 10+9+8+7+6+5+4+3+2+1);
+     
+return 0;
+}
