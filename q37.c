d1400d0e1e1ba2f106fc29687f1fc1ef1b86e623
Aarav-S0406 <aarav.s0406@gmail.com>
2025-09-30 13:21:06 +0530

    Add files via upload

---
+++ q37.c
@@ -0,0 +1,37 @@
+//Q37: Write a program to find the LCM of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+4 5
+Output 1:
+20
+
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+#include <stdio.h>
+
+int main() {
+
+    int a, b, temp, x, y;
+    int hcf;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    x=a;
+    y=b;
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    hcf=a;
+    int lcm=(x*y)/hcf;
+    
+    printf("LCM= %d\n", lcm);
+    return 0;
+}
